5c786e56813112f5355e13567346ac26ee12c34c
seungdeok <akk9132@naver.com>
2024-02-01 22:00:42 +0900

    feat: Configure msw

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { initMsw } from './api/mocks/initMsw';
+
+initMsw();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
