95c6795b1972b818abb701bb606e4f7f14ac00c3
seungdeok <akk9132@naver.com>
2024-03-13 21:30:23 +0900

    Update api base url

--- src/mocks/initMsw.ts
+++ src/mocks/initMsw.ts
@@ -1,5 +1,8 @@
 export const initMsw = async () => {
-  if (import.meta.env.MODE === 'development') {
+  if (
+    import.meta.env.MODE === 'development' &&
+    import.meta.env.MOCKING_ENABLE === 'true'
+  ) {
     const { worker } = await import('./browser');
     worker.start();
   }
