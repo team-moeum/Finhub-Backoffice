4966d26574e44ab2b73793eeee0284950886a047
seungdeok <akk9132@naver.com>
2024-04-17 01:03:33 +0900

    Feat: Add avatar page

---
+++ src/pages/AvatarPage/List.style.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const pageHeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+
+  div {
+    font-size: 16px;
+    font-weight: 600;
+  }
+`;
+
+export const contentWrapper = styled.div`
+  width: 100%;
+`;
+
+export const listWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+`;
