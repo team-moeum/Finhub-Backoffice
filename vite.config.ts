57dfc996c90c02c2ad89a25b37546295a99510e2
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2024-07-21 20:54:21 +0900

    Configure CICD (#108)
    * chore: configure dockerfile
    
    * chore: build ec2 & ecr & github action yml
    
    * chore: rename docker container name
    
    * chore: update workflow branch
    
    * chore: remove prod workflow
    
    * chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    server: {
-      port: 3001,
-      proxy: {
-        '/api': {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-        },
-      },
-    },
     plugins: [
       react({
         jsxImportSource: '@emotion/react',
@@ -25,6 +15,22 @@ export default defineConfig(({ mode }) => {
       }),
       tsconfigPaths(),
     ],
+    preview: {
+      port: 3001,
+      strictPort: true,
+    },
+    server: {
+      port: 3001,
+      strictPort: true,
+      host: true,
+      origin: 'http://0.0.0.0:3001',
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
     resolve: {
       alias: {
         'react-router-dom': 'react-router-dom',
