8e3c096991b16e1261ed7c3c4fd67e1d06105661
seungdeok <akk9132@naver.com>
2024-08-11 16:18:13 +0900

    feat: add voc page

---
+++ src/api/feedback.ts
@@ -0,0 +1,61 @@
+import { FeedbackType } from '@finhub/types/Feedback';
+import { ApiResposne, client } from './client';
+
+const prefix = '/admin/feedback';
+
+const list = async ({
+  page,
+  listSize,
+  reply,
+}: {
+  page: number;
+  listSize: number;
+  reply: string;
+}) => {
+  let url = `${prefix}?page=${page}&size=${listSize}`;
+  if (reply !== '전체') {
+    url += `&reply=${reply}`;
+  }
+  const response: ApiResposne = await client.get(url);
+
+  if (response.status === 'FAIL') {
+    return {
+      list: [],
+      totalDocuments: 0,
+    };
+  }
+
+  const dataSource: {
+    vocList: FeedbackType[];
+    pageInfo: {
+      currentPage: number;
+      totalPages: number;
+      pageSize: number;
+      totalElements: number;
+    };
+  } = response.data;
+
+  return {
+    list: dataSource.vocList,
+    // TODO (API 수정 필요)
+    totalDocuments: dataSource.vocList.length,
+  };
+};
+
+const show = async ({ id }: { id: number }) => {
+  const response: ApiResposne = await client.get(`${prefix}/${id}`);
+  const dataSource = response.data;
+  return dataSource.vocDetail;
+};
+
+const send = async ({ id, text }: { id: number; text: string }) => {
+  const response: ApiResposne = await client.post(`${prefix}`, { id, text });
+  const dataSource = response.data;
+  return dataSource;
+};
+
+export const feedbackAPI = {
+  list,
+  show,
+  send,
+};
---
+++ src/types/Feedback.ts
@@ -0,0 +1,15 @@
+export interface FeedbackType {
+  feedbackId: number;
+  email: string;
+  context: string;
+  reply: string;
+}
+
+export interface FeedbackItemType extends FeedbackType {
+  userAgent: string;
+  appVersion: string;
+  fileUrl1: string | null;
+  fileUrl2: string | null;
+  fileUrl3: string | null;
+  fileUrl4: string | null;
+}
