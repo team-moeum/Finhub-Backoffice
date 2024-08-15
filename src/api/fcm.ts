50af87f934978769d43b05fe3ec25fe7681e335e
seungdeok <akk9132@naver.com>
2024-08-16 00:48:22 +0900

    feat: update sendNoti api interface in fcm page

--- src/api/fcm.ts
+++ src/api/fcm.ts
@@ -1,21 +1,29 @@
 import { ApiResposne, client } from './client';
 
+export interface SendNotiRequestType {
+  type: number;
+  target?: string[];
+  title: string;
+  content: string;
+  view: string;
+  action?: { [key: string]: any };
+}
+
 const sendNoti = async ({
+  type,
   target,
   title,
   content,
   view,
-}: {
-  target: string;
-  title: string;
-  content: string;
-  view: string;
-}) => {
+  action = {},
+}: SendNotiRequestType) => {
   const response: ApiResposne = await client.post('/admin/send-noti', {
+    type,
     target,
     title,
     content,
     view,
+    action,
   });
 
   if (response.status === 'FAIL') {
