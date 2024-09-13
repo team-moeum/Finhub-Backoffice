64f1848853e043662586e7950115faa0fd0818f6
seungdeok <akk9132@naver.com>
2024-09-13 22:11:15 +0900

    feat: add comment report page api rejected & approved

--- src/types/CommentReport.ts
+++ src/types/CommentReport.ts
@@ -1,9 +1,10 @@
 export interface ICommentReport {
   id: number;
   reason: string;
-  useYn: string;
+  isProcessed: string;
   commentId?: number;
   comment?: string;
   reportedNickname?: string;
   reporterNickname?: string;
+  approvalStatus?: string;
 }
